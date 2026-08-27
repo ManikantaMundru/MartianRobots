88b89d35c15d5bb9ffeade827fe1db64025b2ca9
Manikanta Mundru <manikanta.mundru@doctorly.de>
2026-08-27 21:23:00 +0200

    Add tests for robot movement and Mars grid rules

---
+++ tests/MartianRobots.Tests/Domain/MarsGridTests.cs
@@ -0,0 +1,27 @@
+﻿using MartianRobots.Domain;
+using Shouldly;
+
+namespace MartianRobots.Tests.Domain
+{
+    public class MarsGridTests
+    {
+        [Fact]
+        public void IsInside_ShouldRespectGridBoundaries()
+        {
+            // Arrange
+            var grid = new MarsGrid(5, 3);
+
+            // Act
+            var bottomLeft = grid.IsInside(new Position(0, 0));
+            var topRight = grid.IsInside(new Position(5, 3));
+            var outsideRight = grid.IsInside(new Position(6, 3));
+            var outsideLeft = grid.IsInside(new Position(-1, 0));
+
+            // Assert
+            bottomLeft.ShouldBeTrue();
+            topRight.ShouldBeTrue();
+            outsideRight.ShouldBeFalse();
+            outsideLeft.ShouldBeFalse();
+        }
+    }
+}
---
+++ tests/MartianRobots.Tests/Domain/RobotTests.cs
@@ -0,0 +1,69 @@
+﻿using MartianRobots.Domain;
+using Shouldly;
+
+namespace MartianRobots.Tests.Domain
+{
+    public class RobotTests
+    {
+        [Fact]
+        public void TurnRight_ShouldChangeDirectionCorrectly()
+        {
+            // Arrange
+            var robot = new Robot(new Position(1, 1), Direction.North);
+
+            // Act
+            robot.TurnRight();
+
+            // Assert
+            robot.Direction.ShouldBe(Direction.East);
+        }
+
+        [Fact]
+        public void MoveForward_ShouldMoveRobotInCurrentDirection()
+        {
+            // Arrange
+            var grid = new MarsGrid(5, 3);
+            var robot = new Robot(new Position(1, 1), Direction.North);
+
+            // Act
+            robot.MoveForward(grid);
+
+            // Assert
+            robot.Position.ShouldBe(new Position(1, 2));
+            robot.IsLost.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void MoveForward_OutsideGrid_ShouldMarkRobotAsLost()
+        {
+            // Arrange
+            var grid = new MarsGrid(5, 3);
+            var robot = new Robot(new Position(5, 3), Direction.East);
+
+            // Act
+            robot.MoveForward(grid);
+
+            // Assert
+            robot.IsLost.ShouldBeTrue();
+            robot.Position.ShouldBe(new Position(5, 3));
+        }
+
+        [Fact]
+        public void MoveForward_FromScentedPosition_ShouldIgnoreInstruction()
+        {
+            // Arrange
+            var grid = new MarsGrid(5, 3);
+            var firstRobot = new Robot(new Position(5, 3), Direction.East);
+            firstRobot.MoveForward(grid);
+
+            var secondRobot = new Robot(new Position(5, 3), Direction.East);
+
+            // Act
+            secondRobot.MoveForward(grid);
+
+            // Assert
+            secondRobot.IsLost.ShouldBeFalse();
+            secondRobot.Position.ShouldBe(new Position(5, 3));
+        }
+    }
+}
